800ae0902840b1cd51c8249855c7aa90fe83d8a6
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-03-30 19:35:18 +0100

    inserir video e alteracoes

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./video.css"
 
 function Video() {
+    const videoRef = useRef(null)
+    const [play, setplay] = useState(false)
+
+
+        function handdleStart(){
+            if(play){
+                videoRef.current.pause()
+                setplay(false)
+
+            } else{
+                videoRef.current.play()
+                setplay(true)
+
+            }
+        }
+
   return (
     <div className='video'>
         
-            <video className='video__player'
+            <video 
+            className='video__player'
+            ref={videoRef}
+            onClick={handdleStart}
+            loop
             src="https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/brecker2.mp4?alt=media&token=5493f40a-3b0d-4c19-b5dd-a9016cc5a6d1"
         >
 
