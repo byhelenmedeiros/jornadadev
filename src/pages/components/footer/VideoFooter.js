4501be6b4de2a9d1e25eb1b30f010710de318c9e
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-03-31 20:32:56 +0100

    animacao vinil

--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -3,7 +3,21 @@ import "./videoFooter.css"
 
 function VideoFooter() {
   return (
-    <div className='videoFooter'>VideoFooter</div>
+    <div className='videoFooter'>
+        <div className='videoFooter__text'>
+
+            <h3>@Helen medeiros</h3>
+                <p>Descriçao do video</p>
+            <div className='videoFooter__music'>
+                <p>Icone</p>
+                <p>Titulo da musica</p>
+            </div>
+        </div>
+        <img className='videoFooter__record'
+        alt='imagem de um vinil girando'
+        src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png'
+        />
+    </div>
   )
 }
 
