e87972bb81765cec7cb50d0c60aef5ab282bbd77
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-04-02 21:45:24 +0100

    firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNA3uf0toDSpXMyBXA0mBDg2XpC_iCcr0",
+  authDomain: "tiktok-clone-5b239.firebaseapp.com",
+  projectId: "tiktok-clone-5b239",
+  storageBucket: "tiktok-clone-5b239.appspot.com",
+  messagingSenderId: "52231029661",
+  appId: "1:52231029661:web:3f27349b997920a9e3e574"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
