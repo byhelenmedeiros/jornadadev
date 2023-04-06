c6430284f59de6fef3fcb81e82057f0a836bb3d3
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-04-06 21:20:53 +0100

    app js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from "./pages/video";
 import db from "./config/firebase"
+import { collection, getDocs } from 'firebase/firestore/lite';
+
 
 function App() {
-  return (
-    <div className="App">
-      <div className='app__videos'>
-       
-       <Video 
-       likes={87}
-       messages={164}
-       shares={52}
-       name="Helen"
-       description="O gato zé"
-       music="Cat music epic - Music of cat"
-       url="https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/brecker2.mp4?alt=media&token=5493f40a-3b0d-4c19-b5dd-a9016cc5a6d1"
-       />
 
-       <Video 
-       likes={44}
-       messages={24}
-       shares={7}
-       name="Helen"
-       description="O gato zé"
-       music="Cat music fun"
-       url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
-       />
-       
-       <Video 
-       likes={126}
-       messages={47}
-       shares={6}
-       name="Helen"
-       description="O gato zé"
-       music="Cat music fun"
-       />
+    const [video, setVideos]= useState([])
 
-       <Video 
+
+    async function getVideos(){
+      const videosCollection = collection( db, "videos")
+      const videosSnapshot = await getDocs(videosCollection)
+      const videosList = videosSnapshot.docs.maps((doc) => doc.data());
+      setVideos(videosList)
+    }
+
+  useEffect(()=>{
+    getVideos();
+
+ },[])
+
+
+
+    return (
+      <div className="App">
+        <div className='app__videos'>
+        
+        { video.map((item)=> {
+          return(
+              <Video 
+          likes={item.likes}
+          messages={item.messages}
+          shares={item.shares}
+          name={item.name}
+          description= {item.description}
+          music={item.music}
+          url={item.url}
+        />
+          );
+
+        })   }
+
+
+       {/* <Video 
        likes={147}
        messages={63}
        shares={4}
        name="Helen"
        description="O gato zé"
        music="Cat music fun"
-       />
+       /> */}
        
      
 
