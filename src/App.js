62b5ff8cbea24a79836c5f67fff4ecd8472ca8d7
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-04-06 22:09:50 +0100

    permissao no banco de dados

--- src/App.js
+++ src/App.js
@@ -7,56 +7,44 @@ import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
 
-    const [video, setVideos]= useState([])
+  const [video, setVideos] = useState([])
 
 
-    async function getVideos(){
-      const videosCollection = collection( db, "videos")
-      const videosSnapshot = await getDocs(videosCollection)
-      const videosList = videosSnapshot.docs.maps((doc) => doc.data());
-      setVideos(videosList)
-    }
+  async function getVideos() {
+    const videosCollection = collection(db, "videos")
+    const videosSnapshot = await getDocs(videosCollection)
+    const videosList = videosSnapshot.docs.map(doc => doc.data());
+    setVideos(videosList)
+  }
 
-  useEffect(()=>{
+  useEffect(() => {
     getVideos();
 
- },[])
+  }, [])
 
+  return (
+    <div className="App">
+      <div className='app__videos'>
 
-
-    return (
-      <div className="App">
-        <div className='app__videos'>
-        
-        { video.map((item)=> {
-          return(
-              <Video 
-          likes={item.likes}
-          messages={item.messages}
-          shares={item.shares}
-          name={item.name}
-          description= {item.description}
-          music={item.music}
-          url={item.url}
-        />
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
           );
 
-        })   }
+        })}
 
 
-       {/* <Video 
-       likes={147}
-       messages={63}
-       shares={4}
-       name="Helen"
-       description="O gato zé"
-       music="Cat music fun"
-       /> */}
-       
-     
 
-     
-       </div>
+
+      </div>
     </div>
   );
 }
